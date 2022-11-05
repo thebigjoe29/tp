c6c175b90653f8a691ba1ff1853bf62035f80d10
thebigjoe29 <thebigjoe29@gmail.com>
2022-11-05 22:05:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: TextButton(
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource('assets/note1.wav'));
+            },
+            child: Center(
+              child: Text(
+                "press me",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
